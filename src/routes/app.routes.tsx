cf6c0ef0015fa5d3215ef4b98d0843efee4fd6a1
Lucas Cunha <lucasviana112@gmail.com>
2020-09-15 21:13:46 -0300

    creating routes for pages: CreateAppointment, AppointmentCreated and Profile

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+import CreateAppointment from '../pages/CreateAppointment';
+import AppointmentCreated from '../pages/AppointmentCreated';
 
 const App = createStackNavigator();
 
-const AuthRoutes: React.FC = () => (
+const AppRoutes: React.FC = () => (
   <App.Navigator
     screenOptions={{
       headerShown: false,
@@ -13,7 +16,10 @@ const AuthRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointment" component={CreateAppointment} />
+    <App.Screen name="AppointmentCreated" component={AppointmentCreated} />
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
-export default AuthRoutes;
+export default AppRoutes;
